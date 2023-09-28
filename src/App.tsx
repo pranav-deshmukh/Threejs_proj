2f18304ec99a0758b9520b2de08a65b99404370c
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2023-09-28 12:44:41 +0530

    threejs proj setup

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import "./folio/src/index.js"
+
+function ThreeScene() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[5, 5, 5]} intensity={1} />
+      <pointLight position={[-3, -3, 2]} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+function App() {
+  return (
+    <div className="App h-screen">
+      <Canvas>
+        <ThreeScene />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
